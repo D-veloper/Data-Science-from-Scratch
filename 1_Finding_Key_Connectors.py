1a835810727e79fd0c40eda02a83697c0394949f
pcnnadi <psxpn2@nottingham.ac.uk>
2024-05-23 17:19:37 +0100

    finding users with the highest conections/most connectivity

--- 1_Finding_Key_Connectors.py
+++ 1_Finding_Key_Connectors.py
@@ -40,3 +40,29 @@ num_users = len(users)
 avg_connections = total_connections / num_users
 
 print(avg_connections)
+
+# who are the most connected people?
+num_friends_by_id = [(user["id"], number_of_friends(user)) for user in users]  # list user_id, number_of_friends
+
+# since there aren't many users, we'll sort them from most to the least friends
+num_friends_by_id = sorted(num_friends_by_id, key=lambda user_id_num_friends: user_id_num_friends[1], reverse=True)
+
+highest_connection = num_friends_by_id[0][1]
+users_with_hc = ""
+
+for i, user_connection in enumerate(num_friends_by_id):
+    next_user_connection = num_friends_by_id[i+1]
+    user_name = ""
+    for user in users:
+        if user["id"] == user_connection[0]:
+            user_name = user["name"]
+            break
+    if user_connection[1] == highest_connection:
+        if next_user_connection[1] < highest_connection or i+1 > len(num_friends_by_id):
+            users_with_hc += "and " + user_name + "."
+        else:
+            users_with_hc += user_name + ", "
+    else:
+        break
+
+print(f"The users with the highest number of connections are {users_with_hc}")
