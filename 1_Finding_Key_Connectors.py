3f5c7cbec57d9cb0c2809a6f255cca09886e54b3
pcnnadi <psxpn2@nottingham.ac.uk>
2025-02-04 11:22:09 +0000

    friends by interests

--- 1_Finding_Key_Connectors.py
+++ 1_Finding_Key_Connectors.py
@@ -1,5 +1,5 @@
 from __future__ import division  # apparently, integer division is lame lol
-from collections import Counter
+from collections import Counter, defaultdict
 
 # list of users, detail of each user stored as dict allowing easy modification later
 users = [
@@ -15,6 +15,24 @@ users = [
     {"id": 9, "name": "Klein"}
 ]
 
+interests = [
+    (0, "Hadoop"), (0, "Big Data"), (0, "HBase"), (0, "Java"),
+    (0, "Spark"), (0, "Storm"), (0, "Cassandra"),
+    (1, "NoSQL"), (1, "MongoDB"), (1, "Cassandra"), (1, "HBase"),
+    (1, "Postgres"), (2, "Python"), (2, "scikit-learn"), (2, "scipy"),
+    (2, "numpy"), (2, "statsmodels"), (2, "pandas"), (3, "R"), (3, "Python"),
+    (3, "statistics"), (3, "regression"), (3, "probability"),
+    (4, "machine learning"), (4, "regression"), (4, "decision trees"),
+    (4, "libsvm"), (5, "Python"), (5, "R"), (5, "Java"), (5, "C++"),
+    (5, "Haskell"), (5, "programming languages"), (6, "statistics"),
+    (6, "probability"), (6, "mathematics"), (6, "theory"),
+    (7, "machine learning"), (7, "scikit-learn"), (7, "Mahout"),
+    (7, "neural networks"), (8, "neural networks"), (8, "deep learning"),
+    (8, "Big Data"), (8, "artificial intelligence"), (9, "Hadoop"),
+    (9, "Java"), (9, "MapReduce"), (9, "Big Data")
+]
+
+
 # list of connections
 friendships = [(0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (3, 4),
                (4, 5), (5, 6), (5, 7), (6, 8), (7, 8), (8, 9)]
@@ -23,7 +41,7 @@ friendships = [(0, 1), (0, 2), (1, 2), (1, 3), (2, 3), (3, 4),
 for user in users:
     user["friends"] = []
 
-# note each user's connection
+# note each user's connection and populate their friendship list
 for i, j in friendships:
     users[i]["friends"].append(users[j])
     users[j]["friends"].append(users[i])
@@ -34,7 +52,8 @@ def number_of_friends(user):  # find total number of connections by summing leng
     return len(user["friends"])  # how many friends does user have?
 
 
-total_connections = sum(number_of_friends(user) for user in users)
+# average is sum of elements divided by number of elements
+total_connections = sum(number_of_friends(user) for user in users)  # find sum of connections
 
 # the average is simply total connections divided by number of users
 num_users = len(users)
@@ -99,4 +118,36 @@ def friends_of_friend_ids(user):
                    if not_the_same(user, foaf)  # who aren't me
                    and not_friends(user, foaf))  # and aren't my friends
 
+
 print(friends_of_friend_ids(users[3]))
+
+
+# Function to find users with certain interests
+def data_scientists_who_like(target_interest):
+    return[user_id
+           for user_id, user_interest in interests
+           if user_interest == target_interest]
+
+
+# ↑ works. But has to examine the whole list of interests, for every search.
+# Building an index from users to interest and interest to users is better if we want to do more searches
+
+# keys are interests, values are lists of user_ids with that interest
+user_ids_by_interest = defaultdict(list)
+
+for user_id, interest in interests:
+    user_ids_by_interest[interest].append(user_id)
+
+# keys are user_ids, values are lists of interests, for that user_id
+interests_by_user_id = defaultdict(list)
+
+for user_id, interest in interests:
+    interests_by_user_id[user_id].append(interest)
+
+# Question: Who has the most interests in common with a given user?
+
+def most_common_interest_with(user):
+    return Counter(interested_user_id  # keep count of how many times we see each user
+                   for interest in interests_by_user_id[user["id"]]  # iterate over the user's interests
+                   for interested_user_id in user_ids_by_interest[interest]  # for each interest, iterate over the users with the same interest
+                   if interested_user_id != user["id"])
